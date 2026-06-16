a1a47af28ebe3f5eb9a501b2446ccb186771d5ad
Pramod Tarpe <tarpe2000@gmail.com>
2026-06-16 11:20:36 +0530

    QWEN MAJOR UI OVERHAUL

--- src/component/subnav-menu/subnav-menu.component.spec.ts
+++ src/component/subnav-menu/subnav-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SubnavMenuComponent } from './subnav-menu.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 describe('SubnavMenuComponent', () => {
   let component: SubnavMenuComponent;
@@ -8,10 +9,9 @@ describe('SubnavMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SubnavMenuComponent]
-    })
-    .compileComponents();
-    
+      imports: [SubnavMenuComponent, CommonModule, RouterLink, RouterLinkActive]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SubnavMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,17 @@ describe('SubnavMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start in expanded state by default', () => {
+    expect(component.collapsed).toBe(false);
+  });
+
+  it('should toggle collapsed state on toggleCollapse call', () => {
+    component.toggleCollapse();
+    expect(component.collapsed).toBe(true);
+
+    component.toggleCollapse();
+    expect(component.collapsed).toBe(false);
   });
 });
--- src/component/subnav-menu/subnav-menu.component.ts
+++ src/component/subnav-menu/subnav-menu.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT, CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ga-subnav-menu',
@@ -10,5 +10,16 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./subnav-menu.component.scss']
 })
 export class SubnavMenuComponent {
-  // Session logic has been successfully migrated to the Header Component
+  collapsed = false;
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    const sidebarWidth = this.collapsed ? '72px' : '260px';
+    this.document.documentElement.style.setProperty('--sidebar-width', sidebarWidth);
+  }
 }
